680bc2cb1488edff9b2c6faad2337158521270c4
Tabatha Roman <romantabatha17@gmail.com>
2020-09-13 14:50:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -16,6 +16,19 @@
     
 #   }
 
-
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
